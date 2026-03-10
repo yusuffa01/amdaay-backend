27a183f15745a92e2276bf52cad0f1769e5bd525
yusuffa01 <80820447+yusuffa01@users.noreply.github.com>
2026-03-10 17:43:53 +0700

    Update ID User jadi UUID acak

--- models/user.go
+++ models/user.go
@@ -1,11 +1,21 @@
 package models
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type User struct {
-	ID           uint   `gorm:"primaryKey"`
+	ID           string `gorm:"primaryKey;type:varchar(50)"` 
 	Nama         string `gorm:"type:varchar(100);not null"`
 	Email        string `gorm:"type:varchar(100);unique;not null"`
 	Password     string `gorm:"type:varchar(255);not null"`
 	Role         string `gorm:"type:varchar(20);default:'user'"`
 	TokenReset   string `gorm:"type:varchar(100)"`
 	ExpiredReset int64
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = uuid.NewString()
+	return
 }
