36563347550d1a81dad3302b2a5106eff192297d
yusuffa01 <80820447+yusuffa01@users.noreply.github.com>
2026-03-10 18:46:55 +0700

    Fix CORS

--- main.go
+++ main.go
@@ -20,14 +20,14 @@ err := godotenv.Load()
 
 	r := gin.Default()
 
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
+    r.Use(cors.New(cors.Config{
+        AllowOriginFunc:  func(origin string) bool { return true },
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    }))
 
 	r.Static("/uploads", "./uploads")
 	r.GET("/api/menu", controllers.GetMenus)
